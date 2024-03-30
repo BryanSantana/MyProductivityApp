3b01118b93d2d7597cf7ef3b25bb1964883eef34
BryanSantana <bryansantana0210@gmail.com>
2024-03-30 00:59:41 -0400

    Added functionality to left arrow, right arrow icons that increment and decrement the date

--- screens/DayPlannerScreen.js
+++ screens/DayPlannerScreen.js
@@ -1,29 +1,31 @@
 import React, { useState } from 'react';
-import { StyleSheet, View, Button, StatusBar, Text } from 'react-native';
-import {PlusCircleOutlined} from '@ant-design/icons'
+import { StyleSheet, View, Button, StatusBar, Text, TouchableOpacity } from 'react-native';
+import AntDesign from 'react-native-vector-icons/AntDesign';
 export default function DayPlannerScreen() {
     const [date, setDate] = useState(new Date());
-
+    const IconButton = ({ onPress, iconName }) => (
+        <TouchableOpacity style={styles.button} onPress={onPress}>
+          <AntDesign name={iconName} size={30} color="#000" />
+        </TouchableOpacity>
+    );
     const incrementDate = (increment) => {
         setDate(new Date(date.setDate(date.getDate() + (increment ? 1 : -1))));
     };
 
     return (
             <View style={styles.dateRow}>
-                <Button
-                    title="Left"
-                    color="#000000"
-                    onPress={() => incrementDate(false)}
+                <IconButton
+                     onPress={() => incrementDate(false)}
+                     iconName="arrowleft"
                 />
                 
                 {/* Date text */}
                 <Text style={styles.dateText}>{date.toLocaleDateString()}</Text>
                 
                 {/* Right button */}
-                <Button
-                    title="Right"
-                    color="#000000"
+                <IconButton
                     onPress={() => incrementDate(true)}
+                    iconName="arrowright"
                 />
             <StatusBar style="auto" />
         </View>
